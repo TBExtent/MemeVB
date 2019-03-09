7b8cc4bcc948a06bb8378038bc3065bf1b9ca412
TBExtent <jedpreist@gmail.com>
2019-03-09 02:18:14 +0000

    ut begin

---
+++ Assets/MemeControlScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemeControlScript : MonoBehaviour
+{
+    public GameObject meme;
+    public int memeCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < memeCount; i++) {
+            Instantiate(meme);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/memeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class memeScript : MonoBehaviour
+{
+    public int minX, maxX, minY, maxY, minZ, maxZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Memes");
+        if (sprites.Length == 0) Debug.LogError("No memes found");
+        else {
+            int memePos = Mathf.FloorToInt(Random.value * sprites.Length);
+            while (memePos == sprites.Length) memePos = Mathf.FloorToInt(Random.value * sprites.Length);
+            GetComponent<SpriteRenderer>().sprite = sprites[memePos];
+            transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+            transform.rotation = Random.rotation;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
